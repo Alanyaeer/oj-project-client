c7b7b33273d7bf694f8ae3f663241e1b5852605c
Alan <1563085328@qq.com>
2024-02-04 09:35:58 +0800

    solve the problem of package

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()]
     })
   ],
-  base: './',
+  base: '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
